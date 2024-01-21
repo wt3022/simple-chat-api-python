5c2c5e3eb4a4bcc171e6c7b70c9e1269f522f898
wt3022 <>
2024-01-21 15:30:42 +0900

    [update] 試し書き

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"dagger.io/dagger"
@@ -17,5 +16,23 @@ func main() {
 	}
 	defer client.Close()
 
-	fmt.Println("hogehoge")
+	mysqlPassword := "hogehoge"
+	mysql := client.Container().
+		From("mysql:8").
+		WithExposedPort(3306).
+		WithEnvVariable("MYSQL_ROOT_PASSWORD", mysqlPassword).
+		AsService()
+
+	client.Container().
+		From("ghcr.io/wt3022/simple-chat-api-python/django:0.1").
+		WithDirectory("./", client.Host().Directory("./apps"), dagger.ContainerWithDirectoryOpts{Owner: "django"}).
+		WithServiceBinding("db", mysql).
+		WithEnvVariable("DB_PASSWORD", mysqlPassword).
+		WithEnvVariable("DB_HOST", "db").
+		WithEnvVariable("DB_USERNAME", "root").
+		WithEnvVariable("DB_NAME", "db_dev").
+		WithEnvVariable("DB_PORT", "3306").
+		WithExec([]string{"pip", "install", "-U", "-r", "requirements.txt"}).
+		WithExec([]string{"cp", "sample.env", ".env"}).
+		WithExec([]string{"pytest"})
 }
